80593830e5249b7aae67c3ba6b6e9b0a2648026c
simon <simonctemple@live.com>
2016-03-18 14:06:19 -0700

    list all repositories

--- js/get_repos.js
+++ js/get_repos.js
@@ -1,16 +1,23 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(search){
-
+  //get user rinfo
   $.get('https://api.github.com/users/' + search + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
     $("#showUserUsername").text(response.login);
     $("#showUserName").text(response.name);
     $("#showUserGithubEmail").html("<a href='" + response.html_url + "'>" + response.html_url + "</a>");
     $("#showUserEmail").text(response.email);
+    //repo info
+    $.get('https://api.github.com/users/' + response.login + '/repos' + '?access_token=' + apiKey + '&perpage=1000').then(function(repoResponse){
+      var repoResponseInfo = [];
+      for (var i = 0; i < repoResponse.length; i++) {
+        repoResponseInfo[i] = repoResponse[i].html_url;
+        $("#showAllUserRepos").append("<li>" + repoResponseInfo[i] + "</li>");
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
   }).fail(function(error){
     console.log(error.responseJSON.message);
-    $(".showUserUsername").text("Sorry, we couldn't find anyone with that username.");
   });
-
 };
