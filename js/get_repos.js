499d2afa65a61c6571d5df0338c20f72d8cb6b09
simon <simonctemple@live.com>
2016-03-18 12:07:40 -0700

    hide show works

--- js/get_repos.js
+++ js/get_repos.js
@@ -4,12 +4,10 @@ exports.getRepos = function(search){
 
   $.get('https://api.github.com/users/' + search + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-
-      $("#showUserUsername").text(response.login);
-      $("#showUserName").text(response.name);
-      $("#showUserGithubEmail").html("<a href='" + response.html_url + "'>" + response.html_url + "</a>");
-      $("#showUserEmail").text(response.email);
-      // console.log(response.html_url);
+    $("#showUserUsername").text(response.login);
+    $("#showUserName").text(response.name);
+    $("#showUserGithubEmail").html("<a href='" + response.html_url + "'>" + response.html_url + "</a>");
+    $("#showUserEmail").text(response.email);
   }).fail(function(error){
     console.log(error.responseJSON.message);
     $(".showUserUsername").text("Sorry, we couldn't find anyone with that username.");
--- js/search-interface.js
+++ js/search-interface.js
@@ -2,11 +2,11 @@ var getRepos = require('./../js/get_repos.js').getRepos;
 
 $(document).ready(function(){
   $("#button").click(function(event){
+    event.preventDefault();
 
     var searchedName = $("#userName").val();
-
     getRepos(searchedName);
-
+    $(".showUserInfo").show();
 
   });
 });
