0836f8d9472cbc3b18f2ad0285802f97159b345f
simon <simonctemple@live.com>
2016-03-18 12:00:05 -0700

    display all user info to page

--- js/get_repos.js
+++ js/get_repos.js
@@ -3,18 +3,16 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(search){
 
   $.get('https://api.github.com/users/' + search + '?access_token=' + apiKey).then(function(response){
-    if(response.name === null){
-      $(".showUserInfo").text("Sorry, we couldn't find anyone with that username.")
-    }
-    else{
-    $(".showUserInfo").text(response.name);
-    $(".showUserInfo").text(response.name);
-    $(".showUserInfo").text(response.name);
-    $(".showUserInfo").text(response.name);
-    console.log(response.name);
-  }
+    console.log(response);
+
+      $("#showUserUsername").text(response.login);
+      $("#showUserName").text(response.name);
+      $("#showUserGithubEmail").html("<a href='" + response.html_url + "'>" + response.html_url + "</a>");
+      $("#showUserEmail").text(response.email);
+      // console.log(response.html_url);
   }).fail(function(error){
     console.log(error.responseJSON.message);
+    $(".showUserUsername").text("Sorry, we couldn't find anyone with that username.");
   });
 
 };
