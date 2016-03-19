4b2034b3b71d875a69d7aab6331acb211c6e9365
Simon Temple <simonctemple@live.com>
2016-03-19 00:35:49 -0700

    remove comments, console.logs, and spaces.

--- js/get_repos.js
+++ js/get_repos.js
@@ -1,13 +1,10 @@
 var apiKey = require('./../.env').apiKey;
-
 exports.getRepos = function(search){
-  //get user rinfo
   $.get('https://api.github.com/users/' + search + '?access_token=' + apiKey + '&per_page=1000').then(function(response){
     $("#showUserUsername").text(response.login);
     $("#showUserName").text(response.name);
     $("#showUserGithubEmail").html("<a href='" + response.html_url + "'>" + response.html_url + "</a>");
     $("#showUserEmail").text(response.email);
-    //repo info
     $.get('https://api.github.com/users/' + response.login + '/repos' + '?access_token=' + apiKey + '&per_page=1000').then(function(repoResponse){
       console.log(repoResponse);
       var repoResponseName = [];
@@ -17,7 +14,6 @@ exports.getRepos = function(search){
         repoResponseDescription[i] = repoResponse[i].description;
         $("#showAllUserRepos").append("<li> Name: " + repoResponseName[i] + " - Description: " + repoResponseDescription[i] + "</li>");
       }
-
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
